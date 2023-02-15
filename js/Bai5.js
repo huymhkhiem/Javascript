75c520f59426d13b2979d90caefd7d048d364f55
ZenKoi <khiemb1505831@gmail.com>
2023-02-15 15:24:07 +0700

    Bai5 String lần 2

--- js/Bai5.js
+++ js/Bai5.js
@@ -1,1 +1,7 @@
 console.log("Hello".toUpperCase());
+let a = 5 ;
+let b =6.2;
+let d =  '5';
+let e = Number(d);
+console.log('d value: ', d, 'type :', typeof d );
+console.log('e value:', e, 'type :', typeof e);
